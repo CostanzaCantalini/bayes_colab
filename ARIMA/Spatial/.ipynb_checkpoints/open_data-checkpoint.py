b97ad1f9d24d229e52b14f484b480690d84d829e
bruno-polimi <bruno.ursino@mail.polimi.it>
2023-02-10 22:00:24 +0100

    new ARIMA samples with corrected covariates

--- ARIMA/Spatial/.ipynb_checkpoints/open_data-checkpoint.py
+++ ARIMA/Spatial/.ipynb_checkpoints/open_data-checkpoint.py
@@ -9,13 +9,17 @@ def open():
     df = pd.read_csv('../data/data.csv')
     df.rename(columns={'Unnamed: 0': 'Date'}, inplace=True)
     df.set_index('Date', inplace=True)
-    df.replace(0.0, np.nan, inplace=True)
+    df.replace(0.0, 1.0, inplace=True)
     df.fillna(np.NaN, inplace=True)
     
-    fields = ["alt", "tipo", "zoning"]
-    reg_matrix=pd.read_csv('../data/regression.csv',sep=";",names=fields)
+    reg_matrix=pd.read_csv('../data/regression.csv')    
+    reg_matrix = reg_matrix.drop('Stazione', axis=1)
     reg_matrix.iloc[:,0] = (reg_matrix.iloc[:,0]-reg_matrix.iloc[:,0].mean())/reg_matrix.iloc[:,0].std()
-    dummies = pd.get_dummies(reg_matrix, columns=['tipo','zoning'], drop_first=True)
+    dummies = pd.get_dummies(reg_matrix)
+    dummies = dummies.drop('TipoStazione_Fondo', axis=1)
+    dummies = dummies.drop('TipoArea_Urbano', axis=1)
+    dummies = dummies.drop('Zonizzazione_Pianura Ovest', axis=1)
+    dummies.columns = ['Altitude','Type_Industrial','Type_Traffic','Type_Rural','Type_SubUrban','Zone_Agglomerato','Zone_Appennino','Zone_Est']
 
     coordinates = ["latitude","longitude"]
     coord_matrix = pd.read_csv('../data/coord.csv',sep=",",names=coordinates)
